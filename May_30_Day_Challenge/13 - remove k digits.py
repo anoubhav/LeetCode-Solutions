569fe6f38be404176cbe0a56b9f8ced3ba9cef0b
Anoubhav Agarwaal <anoubhav.agarwaal@gmail.com>
2020-05-28 17:39:21 +0530

    may 13 solution

--- May_30_Day_Challenge/13 - remove k digits.py
+++ May_30_Day_Challenge/13 - remove k digits.py
@@ -1,4 +1,58 @@
-def removeKdigits(num: str, k: int) -> str:
+def removeKdigits(num, k) -> str:
+    # Intuition: remove the first 'peak' digit
+    stack = list()
+
+    if len(num) == k:
+        return '0'
+
+    for i, char in enumerate(num):
+        if not stack:
+            stack.append(num[0])
+        else:
+            if char >= stack[-1]:
+                stack.append(char)
+            else:
+                # char < stack[-1], keep popping until the peak is gone
+                while stack and char < stack[-1]:
+                    if k == 0:
+                        break
+                    k -= 1
+                    stack.pop()
+
+                stack.append(char)
+
+    stack += list(num[i+1:])
+    if k==0:
+        return str(int(''.join(stack)))
+    else:
+        # remove top k elements of stack, while preserving order.
+        # while k!=0:
+        #     stack.remove(max(stack))
+        #     k-=1
+
+        ## The stack will only contain elements in increasing order, coz if local peak exists, it would have been popped earlier.
+        return str(int(''.join(stack[:-k])))
+
+
+print(removeKdigits("1432219", 3))
+print(removeKdigits("14301620", 4))
+print(removeKdigits("143012345", 4))
+print(removeKdigits("1119", 4))
+print(removeKdigits("1119", 1))
+print(removeKdigits("10200", 1))
+print(removeKdigits("10", 2))
+print(removeKdigits("0", 0))
+print(removeKdigits("21", 1))
+print(removeKdigits("372181", 2))
+
+
+
+
+
+
+
+
+def Myincorrect2hrsoln_removeKdigits(num: str, k: int) -> str:
     def no_zeros(num, k):
         indices = [1]*len(num)
         for i, digit in enumerate(num[:-1]):
@@ -55,14 +109,13 @@ def removeKdigits(num: str, k: int) -> str:
         if k!=0:
             return no_zeros(num, k)
 
+# print(removeKdigits('12345', 2))
+# print(removeKdigits("1432219", 3))
+# print(removeKdigits("1119", 1))
+# print(removeKdigits("9119", 2))
 
-print(removeKdigits('12345', 2))
-print(removeKdigits("1432219", 3))
-print(removeKdigits("1119", 1))
-print(removeKdigits("9119", 2))
-
-print(removeKdigits("10200", 1))
-print(removeKdigits("10", 1))
-print(removeKdigits("110099", 3))
-print(removeKdigits("120098", 2))
-print(removeKdigits("1107", 1))
+# print(removeKdigits("10200", 1))
+# print(removeKdigits("10", 1))
+# print(removeKdigits("110099", 3))
+# print(removeKdigits("120098", 2))
+# print(removeKdigits("1107", 1))
